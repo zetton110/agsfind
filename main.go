27afa64596d57325b991c7fcefc89ef6b65deab3
zetton110 <locolv.kohe@gmail.com>
2024-07-15 22:42:53 +0900

    #5 Add create-db prototype in cmd package

--- main.go
+++ main.go
@@ -1,46 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
+	"github.com/zetton110/cmkish-cli/cmd"
 )
 
 func main() {
-	var (
-		suffix string
-	)
-	app := cli.NewApp()
-	app.Name = "Hello xxxx"
-	app.Usage = "Make `Hello xxx` for arbitrary text"
-	app.Version = "0.1.0"
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "suffix, s",
-			Value:       "!!!",
-			Usage:       "text after speaking something",
-			Destination: &suffix,
-			EnvVar:      "SUFFIX",
-		},
-	}
+	app := cli.NewApp()
+	app.Name = "cmkish-cli"
+	app.Usage = ""
+	app.Version = "0.0.0"
 
 	app.Commands = []cli.Command{
 		{
-			Name:  "hello",
-			Usage: "if use set -t or --text",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "text, t",
-					Value: "world",
-					Usage: "hello xxx text",
-				},
-			},
-			Action: func(c *cli.Context) error {
-				fmt.Printf("Hello %s %s\n", c.String("text"), suffix)
-				return nil
-			},
+			Name:   "create-db",
+			Usage:  "",
+			Action: cmd.CreateDB,
 		},
 	}
 
